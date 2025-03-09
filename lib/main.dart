05108816f4b7c0389a96e1a58896589ba41792db
ManRi04 <mriessz04@gmail.com>
2025-03-09 12:56:16 +0100

    Have implemented Layout of porgress bars for running, drinking and eating

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: unused_field, unused_element, use_super_parameters
+
+//import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,22 +17,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color(0xFF0F0B1B),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -56,6 +46,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  final controller = PageController(initialPage:0);
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -69,54 +61,98 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+        body: PageView(
+          controller: controller,
+          children: [const DetailPage(headline: "Heute"),const DetailPage(headline: "Gestern")],)
     );
   }
 }
+class DetailPage extends StatefulWidget {
+  const DetailPage({Key? key, required this.headline}) : super(key:key);
+
+  final String headline;
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsetsDirectional.fromSTEB(32, 48, 32, 32),
+        child: Column(children: [
+          Text(widget.headline, style: TextStyle(fontSize: 48, color: Color(0xFFFFFFFF)),),
+          const TrackingElement(color: Color(0xFFEFB21A), icondata: Icons.directions_run,unit:"m", max:5000),
+          const TrackingElement(color: Color(0xFF1A4CEF), icondata: Icons.local_drink, unit: "ml", max: 3000),
+          const TrackingElement(color: Color(0xFFEF1A5A), icondata: Icons.fastfood, unit: "kcal", max:1800)],
+        )
+    );
+  }
+}
+
+
+
+
+class TrackingElement extends StatefulWidget {
+  const TrackingElement({Key? key, required this.color, required this.icondata , required this.unit ,required this.max}) : super(key:key);
+
+  final Color color;
+  final IconData icondata;
+  final String unit;
+  final double max;
+
+  @override
+  State<TrackingElement> createState() => _TrackingElementState();
+}
+
+class _TrackingElementState extends State<TrackingElement> {
+  int _counter = 0;
+  double _progress =0;
+  void _incrementCounter() {
+    setState(() {
+      _counter+=200;
+      _progress = _counter/widget.max;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: _incrementCounter ,
+        child:Column(
+
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(32, 64, 32, 32),
+
+              child: Row(children: [
+                Icon(widget.icondata,color: Colors.white70, size: 50),
+
+                Text(
+                    "$_counter/${widget.max.toInt()}${widget.unit}",
+                    style: TextStyle(color: Color(0xFFE5F4F3), fontSize: 35)
+                ),]),
+            ),
+            LinearProgressIndicator(
+              value:_progress,
+              color: widget.color,
+              backgroundColor: const Color(0x40FFFFFF),
+              minHeight: 12.0,
+            )
+
+
+          ],
+        ));
+
+  }
+}
+
+
+
+
+
+
